77a602a019f3c1fbde28b621803cd77548b8844a
felix mwendwa <felix.mwendwa@student.moringaschool.com>
2022-09-04 18:21:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let regularExpression = /[\W_]/g
+  let lowerString = word.toLowerCase().replace(regularExpression, ``);
+  let reverandsplit = lowerString.split(``).reverse().join(``);
+  return lowerString === reverandsplit;
 }
 
 /* 
   Add your pseudocode here
+  Step1. Lowercase the string and use the RegularExpression to remove unwanted characters from it
+  Step2. Use chaining method with built-in functions split, reverse and join
+  Step3. Check if reverseStr is strictly equals to lowerString and return a Boolean 
 */
 
 /*
